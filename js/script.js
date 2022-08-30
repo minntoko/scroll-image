6ef648fceca8948211eb4b86671e215e9e8cde2d
minntoko <minntoko0822@gmail.com>
2022-08-30 20:12:55 +0900

    fixed

--- js/script.js
+++ js/script.js
@@ -3,5 +3,4 @@ const height = document.documentElement.clientHeight;
 window.addEventListener('scroll', () => {
   bg.style.transform = 'scale('+ (1.6 - window.pageYOffset * .6 / height)+')';
   bg.style.opacity = 1 - window.pageYOffset / height;
-  console.log(height);
 });
